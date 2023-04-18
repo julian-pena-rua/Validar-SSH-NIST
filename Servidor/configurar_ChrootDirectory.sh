c73d1b5138b87118fd510333f4c4ad3b8c189165
Julian Peña Rúa <julian.pena@hotmail.com>
2023-04-17 22:05:28 -0500

    Actualización de scripts
    Falta terminar script de ChrootDirectory

--- Servidor/configurar_ChrootDirectory.sh
+++ Servidor/configurar_ChrootDirectory.sh
@@ -1,11 +1,11 @@
 # TITULO:       CONFIGURAR SERVIDOR CHROOT
-# AUTOR: 		Julian A. Peña
-# FECHA:		09/04/2023
-# VERSION:	    1.0.0
-# SITIO: 		https://julian-pena-rua.github.io/cv/
+# AUTOR: 	Julian A. Peña
+# FECHA:	09/04/2023
+# VERSION:	1.0.0
+# SITIO: 	https://julian-pena-rua.github.io/cv/
 #
 # DESCRIPCION: 
-#		Automatizacion para configurar servidor ChrootDirectory utilizando 
+#	Automatizacion para configurar servidor ChrootDirectory utilizando 
 #       Protocolo de Transferencia de Archivos SSH o Protocolo de transferencia 
 #       segura de archivos. SFPT proporciona funcionalidades de acceso a archivos, 
 #       tranferencia de ficheros y adminitracion de los archivos sobre cualquier
@@ -30,25 +30,42 @@
 
 # ----------------------------------------------------------------------------------------
 
+# ===================
 # VARIABLES
+# ===================
 
+NombreGrupo='sftp_users'
+NombreUsuario='javier'
+ContrasenaUsuario='123456789'
 
+# ===================
 # OPERACIONES
-
+# ===================
 
 # 1. Crear un grupo
-sudo groupadd  sftp_users
+sudo groupadd  $NombreGrupo
 
-# 2. Asignar grupo secundario (sftp_users) al usuario
-useradd  -G sftp_users -s /sbin/nologin  jack
-passwd 123456789
-
-# 2. Para usuarios ya existentes
-#usermod –G sftp_users -s /sbin/nologin  jack
+# 1.5. Verifica que el usuario exista
+getent passwd $NombreUsuario > /dev/null 2>&1
+if [ $? -eq 0 ]
+then 
+	echo "usuario existente"
+	# 2. Para usuarios ya existentes
+	#usermod –G sftp_users -s /sbin/nologin  jack
+	usermod –G $NombreGrupo  $NombreUsuario
+	echo "Agregado al grupo existosamente"
+else 
+	# 2. Crea usuario y asigna grupo secundario (sftp_users) al usuario
+	echo "el usuario no existe"
+	#sudo useradd  -G sftp_users -s /sbin/nologin  jack --password 123456789
+	sudo useradd  -G $NombreGrupo $NombreUsuario --password $ContrasenaUsuario
+	echo "el usuario ha sido creado y agregado al grupo exitosamente"
+fi
 
 # Nota : Si desea cambiar el directorio de inicio predeterminado de usuarios, 
-# luego use '-D' opción en el comando useradd y usermod y configure el permisos correctos.
+# luego use '-D' opción en el comando useradd y usermod y configure permisos correctos.
 
+echo "Editando archivo sshd_config" 
 # 3. Ahora edite el archivo de configuración "/etc/ssh/sshd_config"
 # --------------------------------------------------------------------
 # = GENERADO AUTOMATICAMENTE
@@ -59,8 +76,21 @@ passwd 123456789
 #ChrootDirectory %h
 #ForceCommand internal-sftp
 
+#sudo sed -i 's/#Subsystem/Subsystem/g' /etc/ssh/sshd_config
+cd /etc/ssh/
+echo '# --------------------------------------------------------------------' >> sshd_config
+echo '# # = GENERADO POR SCRIPT' >> sshd_config
+echo '# --------------------------------------------------------------------' >> sshd_config
+echo 'Match Group '$NombreGrupo' \n\tForceCommand internal-sftp \n\tChrootDirectory /home/%u/'$NombreGrupo' \n\tX11Forwarding no \n\tAllowTcpForwarding no \n' >> sshd_config
+echo "Edicion realizada con exito" 
+cd ..
+cd ..
+
+echo "Reiniciando servicio SSH" 
 # 3. Reiniciar servicio SSH
 # systemctl restart sshd
+sudo systemctl restart ssh
+echo "Reinicio completado exitosamente" 
 
 # 4. Establecer permisos necesarios
 # chmod 755 /home/jack
